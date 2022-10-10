1bdd1fa091ec837063cba657e0eca0961172380f
JOAOPEDRODINIZDEV <115243882+JOAOPEDRODINIZDEV@users.noreply.github.com>
2022-10-10 15:41:56 -0300

    2Questão

---
+++ 2SegundaQuestão.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main() {
+
+    setlocale(LC_ALL, "Portuguese_Brazil ");
+    float valor1, valor2, valor3, soma, media;
+    printf("Calculadora\n\n");
+
+    printf("Coloque um numero:");
+    scanf("%f",&valor1);
+
+    printf("Coloque um numero:");
+    scanf("%f",&valor2);
+
+    printf("Coloque un numero:");
+    scanf("%f",&valor3);
+
+   
+
+    soma=(valor1+valor2+valor3);
+    media=soma/3;
+
+     printf("O valor calculado entre a média Foi %f %f %f e %f",valor1, valor2, valor3, media);
+
+    printf("\n Resultado %f" , media);
+
+    return 0;
+}
